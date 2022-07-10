e7865c25e0eac3642789847d1d51bddf0dc32c72
Cheriethecodingqueen <109016720+Cheriethecodingqueen@users.noreply.github.com>
2022-07-10 16:53:14 -0400

    Add files via upload

---
+++ YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println(" CCC   H   H  EEEEE  RRRR   IIIII  EEEEE");
+        System.out.println("C   C  H   H  E      RR RR    I    E    ");
+        System.out.println("C      H   H  E      RR RR    I    E    ");
+        System.out.println("C      HHHHH  EEE    RRRR     I    EEE  ");
+        System.out.println("C      H   H  E      R RR     I    E    ");
+        System.out.println("C   C  H   H  E      R  R     I    E    ");
+        System.out.println(" CCC   H   H  EEEEE  R   R  IIIII  EEEEE");
+
+    }
+}
+
